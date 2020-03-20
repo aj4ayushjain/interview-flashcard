e71443069963875544c43615e72a8b089624da0f
Ayush Jain <aj4ayushjain@gmail.com>
2020-03-20 13:47:16 +0530

    Updates

---
+++ precache-manifest.85394a86998192849a706d16ae2d25ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8e9408fec3856e835fa401a9bababc",
+    "url": "/interview-flashcard/index.html"
+  },
+  {
+    "revision": "3c7f8b23f22ef7c3e617",
+    "url": "/interview-flashcard/static/css/main.ca02da1d.chunk.css"
+  },
+  {
+    "revision": "89bb3226cc0eb001e2a9",
+    "url": "/interview-flashcard/static/js/2.431d808c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/interview-flashcard/static/js/2.431d808c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c7f8b23f22ef7c3e617",
+    "url": "/interview-flashcard/static/js/main.14273bab.chunk.js"
+  },
+  {
+    "revision": "0c389b05816be915ca7b",
+    "url": "/interview-flashcard/static/js/runtime-main.44715ed3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/interview-flashcard/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/interview-flash/precache-manifest.948837b68c12cd40bb7a04b65eb07d3b.js"
+  "/interview-flashcard/precache-manifest.85394a86998192849a706d16ae2d25ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/interview-flash/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/interview-flashcard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
